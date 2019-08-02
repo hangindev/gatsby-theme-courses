a70301c608e4fc71c77dba9509a136acec28b413
Jason <klleungjason@gmail.com>
2019-08-02 23:01:58 +0800

    Override theme colors

---
+++ demo/src/@hangindev/gatsby-theme-courses/styled/colors.js
@@ -0,0 +1,15 @@
+const colors = {
+  primary: '#009688',
+  primary100: '#B2DFDB',
+  primary300: '#4DB6AC',
+  primary500: '#009688',
+  primary700: '#00796B',
+  primary900: '#004D40',
+  grey100: '#CFD8DC',
+  grey300: '#90A4AE',
+  grey500: '#607D8B',
+  grey700: '#455A64',
+  grey900: '#263238',
+};
+
+export default colors;
