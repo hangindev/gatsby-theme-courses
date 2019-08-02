d051bff2762df93bf61ac11d8a27799ec4b13612
Jason <klleungjason@gmail.com>
2019-08-02 23:44:26 +0800

    Demo override footer.

--- demo/src/@hangindev/gatsby-theme-courses/components/CoursesFooter.js
+++ demo/src/@hangindev/gatsby-theme-courses/components/CoursesFooter.js
@@ -3,7 +3,21 @@ import React from 'react';
 function CoursesFooter() {
   return (
     <footer>
-      <h4>Override footer🏇</h4>
+      <h4>Roadmap</h4>
+      <ul
+        style={{
+          listStyleType: 'none',
+        }}
+      >
+        <li>
+          <span role="img" aria-label="white-sqaure">⬜</span>{" "} 
+          Use Youtube API to read duration
+        </li>
+        <li>
+          <span role="img" aria-label="white-sqaure">⬜</span>{" "}
+          Support restricted access
+        </li>
+      </ul>
     </footer>
   );
 }
---
+++ demo/src/pages/404.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function NotFound() {
+  return (
+    <h1>Not found <span role="img" aria-label="crying face">😢</span></h1>
+  );
+}
+
+export default NotFound;
