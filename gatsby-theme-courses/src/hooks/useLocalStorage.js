d2183b70bbaf743e3055b630f37f0b9d2f35bc2a
Jason <klleungjason@gmail.com>
2019-08-03 00:37:14 +0800

    Check if window is undefined for gatsby build

--- gatsby-theme-courses/src/hooks/useLocalStorage.js
+++ gatsby-theme-courses/src/hooks/useLocalStorage.js
@@ -5,6 +5,7 @@ function useLocalStorage(key, initialValue) {
   // Pass initial state function to useState so logic is only executed once
   const [storedValue, setStoredValue] = useState(() => {
     try {
+      if (typeof window === 'undefined') return initialValue;
       // Get from local storage by key
       const item = window.localStorage.getItem(key);
       // Parse stored json or if none return initialValue
@@ -19,6 +20,7 @@ function useLocalStorage(key, initialValue) {
   // Return a wrapped version of useState's setter function that ...
   // ... persists the new value to localStorage.
   const setValue = value => {
+    if (typeof window === 'undefined') return;
     try {
       // Allow value to be a function so we have same API as useState
       const valueToStore =
